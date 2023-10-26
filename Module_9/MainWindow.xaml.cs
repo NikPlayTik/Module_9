e2eada858b502ce6d8ca09eb7c856734a70cf451
Nikita Anishenco <tanishenco91@gmail.com>
2023-10-26 13:25:50 +0300

    Добавьте файлы проекта.

---
+++ Module_9/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Module_9
+{
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Task> tasks = new ObservableCollection<Task>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddTask_Click(object sender, RoutedEventArgs e)
+        {
+            // Создание новой задачи
+            AddWindow addWindow = new AddWindow();
+            addWindow.ShowDialog();
+                        
+            // Добавление новой задачи в коллекцию задач
+            tasks.Add(addWindow.newTask);
+            taskListView.ItemsSource = tasks;
+            taskListView.Items.Refresh();
+        }
+        private void EditMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Module_9/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_9
+{
+    public class Task
+    {
+        public string Title { get; set; }
+        public string Priority { get; set; }
+        public DateTime DueDate { get; set; }
+        public string ProgressCompleted { get; set; }
+    }
+}
